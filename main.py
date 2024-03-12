ea4e670b2f15b6ef63050a23032e8c272ec4d77f
profKalil <84177112+profKalil@users.noreply.github.com>
2024-03-12 19:09:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Programa para calcular soma de dois números
+
+#Entrada de dados
+numero1 = 49
+numero2 = 2
+
+#Processamento
+resultado = numero1 + numero2
+
+#Saída de dados
+print("O resultado é " + str(resultado))
